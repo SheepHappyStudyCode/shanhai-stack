b2bcb6347b03604e067a63521c76f2e832d7dda7
SheepHappy <yh2870839006@outlook.com>
2025-03-14 21:40:36 +0800

    feat: 创建用户关注相关实体

---
+++ src/main/java/edu/neuq/techhub/domain/entity/UserFollowDO.java
@@ -0,0 +1,55 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package edu.neuq.techhub.domain.entity;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+/**
+ * 用户关注关系表
+ * @TableName sys_user_follow
+ */
+@TableName(value = "sys_user_follow")
+@EqualsAndHashCode(callSuper = true)
+@Data
+public class UserFollowDO extends BaseDO {
+
+    /**
+     * ID
+     */
+    @TableId(type = IdType.AUTO)
+    private Long id;
+
+    /**
+     * 关注者ID
+     */
+    private Long followerId;
+
+    /**
+     * 被关注者ID
+     */
+    private Long followingId;
+
+    /**
+     * 关注状态 1-有效 0-取消
+     */
+    private Integer status;
+}
---
+++ src/main/java/edu/neuq/techhub/mapper/UserFollowMapper.java
@@ -0,0 +1,31 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package edu.neuq.techhub.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import edu.neuq.techhub.domain.entity.UserFollowDO;
+
+/**
+* @author panda
+* @description 针对表【sys_user_follow(用户关注关系表)】的数据库操作Mapper
+* @createDate 2025-03-14 21:36:18
+* @Entity edu.neuq.techhub.domain.entity.UserFollowDO
+*/
+public interface UserFollowMapper extends BaseMapper<UserFollowDO> {
+
+}
